4d905c493a96a4767d00908ed34ec3979d65fd34
God-Love Mekoudim Medjoussi <gmekoudi@c2r1p2.42sp.org.br>
2023-09-07 15:59:46 -0300

    ex02

---
+++ ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmekoudi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/22 18:52:36 by gmekoudi          #+#    #+#             */
+/*   Updated: 2023/08/22 18:52:54 by gmekoudi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		write(1, &letter, 1);
+		letter--;
+	}
+}
